8343609c494861daea136b28858cff046fc2e1a0
Rui Martins <vandroste@gmail.com>
2018-12-19 21:45:46 +0000

    Add 2D Double Buffer

---
+++ Tron/DoubleBuffer2D.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tron
+{
+    public class DoubleBuffer2D<T>
+    {
+        private T[,] current;
+        private T[,] next;
+        private int XDim { get; }
+        private int YDim { get; }
+
+        public DoubleBuffer2D(int x, int y)
+        {
+            XDim = x;
+            YDim = y;
+            current = new T[x, y];
+            next = new T[x, y];
+            Clear();
+        }
+
+        public T this[int x, int y]
+        {
+            get => current[x, y];
+            set => next[x, y] = value;
+        }
+
+        public void Clear()
+        {
+            Array.Clear(next, 0, next.Length);
+        }
+
+        public void Swap()
+        {
+            T[,] temp = current;
+            current = next;
+            next = temp;
+        }
+    }
+}
+}
