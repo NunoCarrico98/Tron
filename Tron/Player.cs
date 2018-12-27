a53d918df4b61bdbbeee215aaa6a740c424a6237
Nuno Carriço <nuno.carrico.98@gmail.com>
2018-12-27 15:46:44 +0000

    Add comments to Program and Player classes

--- Tron/Player.cs
+++ Tron/Player.cs
@@ -2,77 +2,147 @@ using System;
 
 namespace Tron
 {
+	/// <summary>
+	/// Player that deals with all player operations.
+	/// </summary>
     public class Player
     {
+		/// <summary>
+		/// Current player Direction.
+		/// </summary>
         protected PlayerDirections Direction { get; set; }
+		/// <summary>
+		/// Current Player Column
+		/// </summary>
         public int Column { get; private set; }
+		/// <summary>
+		/// Current Player Row
+		/// </summary>
         public int Row { get; private set; }
+		/// <summary>
+		/// Current Player Score
+		/// </summary>
         public int Score { get; private set; }
 
-        public Player(PlayerDirections direction, int column, int row)
+		/// <summary>
+		/// Player Constructor to initiliase player properties
+		/// </summary>
+		/// <param name="direction">Initial player direction.</param>
+		/// <param name="column">Initial player column.</param>
+		/// <param name="row">Initial player row.</param>
+		public Player(PlayerDirections direction, int column, int row)
         {
+			// Initialise player properties
             Direction = direction;
             Column = column;
             Row = row;
             Score = 0;
         }
 
+		/// <summary>
+		/// Method that changes player diretions accoding to player input.
+		/// </summary>
+		/// <param name="ki">Current player input.</param>
         public void ChangeDirection(ConsoleKeyInfo ki)
         {
-            if (ki.Key == ConsoleKey.W || ki.Key == ConsoleKey.UpArrow)
+			// If player inputs a W (player 1) or an UpArrow (player 2) and 
+			// player current player direction is not Down
+			if (ki.Key == ConsoleKey.W || ki.Key == ConsoleKey.UpArrow)
                 if (Direction != PlayerDirections.Down)
+					// Change Direction to Up
                     Direction = PlayerDirections.Up;
-            if (ki.Key == ConsoleKey.S || ki.Key == ConsoleKey.DownArrow)
+			// If player inputs a S (player 1) or an DownArrow (player 2) and
+			// player current player direction is not Up
+			if (ki.Key == ConsoleKey.S || ki.Key == ConsoleKey.DownArrow)
                 if (Direction != PlayerDirections.Up)
-                    Direction = PlayerDirections.Down;
-            if (ki.Key == ConsoleKey.A || ki.Key == ConsoleKey.LeftArrow)
+					// Change Direction to Down
+					Direction = PlayerDirections.Down;
+			// If player inputs an A (player 1) or an LeftArrow (player 2) and
+			// player current player direction is not Right
+			if (ki.Key == ConsoleKey.A || ki.Key == ConsoleKey.LeftArrow)
                 if (Direction != PlayerDirections.Right)
-                    Direction = PlayerDirections.Left;
-            if (ki.Key == ConsoleKey.D || ki.Key == ConsoleKey.RightArrow)
+					// Change Direction to Left
+					Direction = PlayerDirections.Left;
+			// If player inputs a D (player 1) or an RightArrow (player 2) and
+			// player current player direction is not Left
+			if (ki.Key == ConsoleKey.D || ki.Key == ConsoleKey.RightArrow)
                 if (Direction != PlayerDirections.Left)
-                    Direction = PlayerDirections.Right;
+					// Change Direction to Right
+					Direction = PlayerDirections.Right;
         }
 
+		/// <summary>
+		/// Method that moves the player depending on the Direction.
+		/// </summary>
         public virtual void Move()
         {
+			// If Direction is Right, go right on map
             if (Direction == PlayerDirections.Right)
                 Column++;
-            if (Direction == PlayerDirections.Left)
+			// If Direction is Left, go left on map
+			if (Direction == PlayerDirections.Left)
                 Column--;
-            if (Direction == PlayerDirections.Up)
+			// If Direction is Up, go up on map
+			if (Direction == PlayerDirections.Up)
                 Row--;
-            if (Direction == PlayerDirections.Down)
+			// If Direction is Down, go down on map
+			if (Direction == PlayerDirections.Down)
                 Row++;
         }
 
+		/// <summary>
+		/// Method that detects player collisions between themselves and the 
+		/// world.
+		/// </summary>
+		/// <param name="gameWorld">Current world level.</param>
+		/// <returns>Returns true if a collsion is detected.</returns>
         public bool DetectCollision(bool[,] gameWorld)
         {
+			// Create bool to return and get world x and y size
             bool lost = false;
 			int xdim = gameWorld.GetLength(0);
 			int ydim = gameWorld.GetLength(1);
 
+			// If player position is of the map
             if (Row >= xdim || Row < 0 || Column >= ydim || Column < 0)
                 lost = true;
+			// If player collides with another player
 			else if (gameWorld[Row, Column])
 				lost = true;
 			
+			// Return false if there no collision or true if there is a collision
             return lost;
         }
 
+		/// <summary>
+		/// Method to increase player score.
+		/// </summary>
 		public void IncreaseScore()
 		{
+			// Increase Score by 1
 			Score++;
 		}
 
+		/// <summary>
+		/// Method to reset player properties.
+		/// </summary>
+		/// <param name="direction">Initial player direction.</param>
+		/// <param name="row">Initial player column.</param>
+		/// <param name="col">Initial player row.</param>
 		public virtual void Reset(PlayerDirections direction, int row, int col)
 		{
+			// Reset player properties
 			Direction = direction;
 			Row = row;
 			Column = col;
 		}
 
+		/// <summary>
+		/// Method to reset the player score.
+		/// </summary>
         public void ResetScore()
         {
+			// Reset Score
             Score = 0;
         }
     }
--- Tron/Program.cs
+++ Tron/Program.cs
@@ -1,13 +1,17 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 
 namespace Tron
 {
+	/// <summary>
+	/// Program Class.
+	/// </summary>
 	public class Program
 	{
+		/// <summary>
+		/// Main Method.
+		/// </summary>
+		/// <param name="args">Command Line Arguments.</param>
 		static void Main(string[] args)
 		{
 			// THESE ARE THE CORRECT VALUES FOR AN
@@ -23,20 +27,25 @@ namespace Tron
 
             // Render game view
             Renderer renderer = new Renderer();
+			// Create a new input system
             InputSystem input = new InputSystem();
+			// Create and initialise input thread
             Thread inputThread = new Thread(input.GetUserInput);
 
             // Initialize game
             Tron t = new Tron(Console.WindowHeight - 6, Console.WindowWidth/2 - 1, 
                 renderer, input);
 
+			// Cursor becomes invisible
             Console.CursorVisible = false;
 
+			// Start input thread
             inputThread.Start();
 
             // Start game passing a value of 50ms per frame
             t.MainMenu();
 
+			// Wait for the input thread to join the "main" thread
             inputThread.Join();
 		}
 	}
